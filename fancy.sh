2e678a21e58a27def0aa1e864822d373a33f0c97
Chris McKenzie <kristopolous@yahoo.com>
2026-01-09 05:03:51 -0800

    updates

--- fancy.sh
+++ fancy.sh
@@ -1,4 +1,4 @@
-llc()        { llcat -s "$LLC_SERVER" -k "$LLC_KEY" -m "$LLC_MODEL" "$@" }
-llc-key()    { LLC_KEY=$1 }
+llc()        { llcat -m "$LLC_MODEL" -s "$LLC_SERVER" -k "$LLC_KEY" "$@" }
+llc-model()  { LLC_MODEL=$(llcat -m  -s "$LLC_SERVER" -k "$LLC_KEY" | fzf) }
 llc-server() { LLC_SERVER=$1 }
-llc-models() { LLC_MODEL=$(llcat -s "$LLC_SERVER" -k "$LLC_KEY" -m | fzf) }
+llc-key()    { LLC_KEY=$1 }

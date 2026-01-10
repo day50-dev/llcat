92ae43d3c886e746db5fbea2b0efbfe3f7998d70
Chris McKenzie <kristopolous@yahoo.com>
2026-01-10 03:58:36 -0800

    a little cleanup

--- examples/conversation.sh
+++ examples/conversation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 conv=${CONV:-$(mktemp)}
-jq -r '.[] | "**\(.role)**: \(.content)"' $conv | sd
-echo $conv
-while read -E -p "> " query; do 
+echo -e "  Using: $conv\n"
+jq -r '.[] | "\n**\(.role)**: \(.content)"' $conv | sd
+while read -E -p "  >> " query; do 
     llcat -c $conv "$@" "$query" |& sd
     echo
 done

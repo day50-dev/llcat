92423a77cf3b96b731d1adeb618fb215964f5104
Chris McKenzie <kristopolous@yahoo.com>
2026-01-10 02:55:31 -0800

    stylizing the convo example

--- examples/conversation.sh
+++ examples/conversation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-conv=$(mktemp)
-echo "Welcome to cutting edge technology!"
+conv=${CONV:-$(mktemp)}
+jq -r '.[] | "**\(.role)**: \(.content)"' $conv | sd
+echo $conv
 while read -E -p "> " query; do 
     llcat -c $conv "$@" "$query" |& sd
+    echo
 done

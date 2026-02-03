1133745653f316257ed7bed4a0c7ddc478fd87f8
Chris McKenzie <kristopolous@yahoo.com>
2026-02-02 18:11:34 -0800

    only including the last 100 lines for the convo

--- examples/conversation.sh
+++ examples/conversation.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 conv=${CONV:-$(mktemp)}
 echo -e "  Using: $conv\n"
-jq -r '.[] | "\n**\(.role)**: \(.content)"' $conv | sd
+jq -r '.[] | "\n**\(.role)**: \(.content)"' $conv | tail -100 | sd
 while read -E -p "  >> " query; do 
     llcat -c $conv "$@" "$query" |& sd
     echo
